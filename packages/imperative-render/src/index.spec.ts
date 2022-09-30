ff316fc42e947d4a50bdc3dec5cde05fca13ee3c
Nick Lucas <me@nicklucas.co.uk>
2022-09-30 17:28:20 +0100

    WIP: add tests

---
+++ packages/imperative-render/src/index.spec.ts
@@ -0,0 +1,10 @@
+import { createInstance } from '.'
+
+describe('imperative-render', () => {
+  describe('createInstance', () => {
+    it('creates an instance', () => {
+      const instance = createInstance()
+      // instance.
+    })
+  })
+})
