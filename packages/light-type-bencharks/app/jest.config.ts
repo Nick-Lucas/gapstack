0c99547daaeebd3a2fc6d3fe4755f6b9f0e523ae
Nick Lucas <me@nicklucas.co.uk>
2023-02-10 14:31:22 +0000

    Add benchmarking projects

---
+++ packages/light-type-bencharks/app/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'light-type-bencharks-app',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../coverage/packages/light-type-bencharks/app',
+}
---
+++ packages/light-type-bencharks/app/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css'
+import NxWelcome from './nx-welcome'
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="light-type-bencharks-app" />
+      <div />
+    </>
+  )
+}
+
+export default App
