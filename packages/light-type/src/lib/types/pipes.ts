03b51b5ab5bc02170f942dac303fbee531853e5e
Nick Lucas <me@nicklucas.co.uk>
2023-02-22 22:36:52 +0000

    Update piping to soften the input type when passing a LightType instance

--- packages/light-type/src/lib/types/pipes.ts
+++ packages/light-type/src/lib/types/pipes.ts
@@ -4,6 +4,7 @@ import { ChainableType } from '../chainable/ChainableType'
 import { LightTypeContext } from '../context/LightTypeContext'
 import { LightType } from './LightType'
 import { TypeInner } from './TypeInner'
+import { SoftenInput } from './utils'
 
 export type PipeFunc<TInput = any, TOutput = any> = (
   input: TInput,
@@ -14,54 +15,54 @@ export type PipeType<TInput = any, TOutput = any> = LightType<TInput, TOutput>
 
 export type PipeElem<TInput = any, TOutput = any> =
   | PipeFunc<TInput, TOutput>
-  | PipeType<TInput, TOutput>
+  | PipeType<SoftenInput<TInput>, TOutput>
 
-export function createPipeFunction<TInput, TOutput>(
-  t: TypeInner<TInput, TOutput>
+export function createPipeFunction<TInitialInput, TInitialOutput>(
+  t: TypeInner<TInitialInput, TInitialOutput>
 ) {
-  function pipe<A extends TOutput, TFinalOutput>(
+  function pipe<A extends TInitialOutput, TFinalOutput>(
     a: PipeElem<A, TFinalOutput>
-  ): ChainableType<TInput, TFinalOutput>
+  ): ChainableType<TInitialInput, TFinalOutput>
 
-  function pipe<A extends TOutput, B, TFinalOutput>(
+  function pipe<A extends TInitialOutput, B, TFinalOutput>(
     a: PipeElem<A, B>,
     b: PipeElem<B, TFinalOutput>
-  ): ChainableType<TInput, TFinalOutput>
+  ): ChainableType<TInitialInput, TFinalOutput>
 
-  function pipe<A extends TOutput, B, C, TFinalOutput>(
+  function pipe<A extends TInitialOutput, B, C, TFinalOutput>(
     a: PipeElem<A, B>,
     b: PipeElem<B, C>,
     c: PipeElem<C, TFinalOutput>
-  ): ChainableType<TInput, TFinalOutput>
+  ): ChainableType<TInitialInput, TFinalOutput>
 
-  function pipe<A extends TOutput, B, C, D, TFinalOutput>(
+  function pipe<A extends TInitialOutput, B, C, D, TFinalOutput>(
     a: PipeElem<A, B>,
     b: PipeElem<B, C>,
     c: PipeElem<C, D>,
     d: PipeElem<D, TFinalOutput>
-  ): ChainableType<TInput, TFinalOutput>
+  ): ChainableType<TInitialInput, TFinalOutput>
 
-  function pipe<A extends TOutput, B, C, D, E, TFinalOutput>(
+  function pipe<A extends TInitialOutput, B, C, D, E, TFinalOutput>(
     a: PipeElem<A, B>,
     b: PipeElem<B, C>,
     c: PipeElem<C, D>,
     d: PipeElem<D, E>,
     e: PipeElem<E, TFinalOutput>
-  ): ChainableType<TInput, TFinalOutput>
+  ): ChainableType<TInitialInput, TFinalOutput>
 
-  function pipe<A extends TOutput, B, C, D, E, F, TFinalOutput>(
+  function pipe<A extends TInitialOutput, B, C, D, E, F, TFinalOutput>(
     a: PipeElem<A, B>,
     b: PipeElem<B, C>,
     c: PipeElem<C, D>,
     d: PipeElem<D, E>,
     e: PipeElem<E, F>,
     f: PipeElem<F, TFinalOutput>
-  ): ChainableType<TInput, TFinalOutput>
+  ): ChainableType<TInitialInput, TFinalOutput>
 
   // TODO: add more overloads 🙂
 
   function pipe(...funcs: PipeElem[]) {
-    return new ChainableType<TInput, any>({
+    return new ChainableType<TInitialInput, any>({
       parse(input, ctx) {
         const nextInput = t.parse(input, ctx)
 
