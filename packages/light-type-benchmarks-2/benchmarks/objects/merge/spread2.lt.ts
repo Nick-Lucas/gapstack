27a5afdee01843b75fa48e3220d0ea63a75f487e
Nick Lucas <me@nicklucas.co.uk>
2023-02-25 14:51:49 +0000

    Add alternative spread benchmarks for lt and zod

---
+++ packages/light-type-benchmarks-2/benchmarks/objects/merge/spread2.lt.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { lt } from 'dist-lt'
+
+export const EntityDto = lt.object({
+  id: lt.number(),
+})
+
+export const PersonDto = lt.object({
+  // @ts-ignore
+  id: lt.string(),
+  firstName: lt.string(),
+  lastName: lt.string(),
+  tel: lt.string().optional(),
+  ...EntityDto.shape,
+})
+
+export const CarDto = lt.object({
+  // @ts-ignore
+  id: lt.string(),
+  name: lt.string(),
+  age: lt.number(),
+  brand: lt
+    .literal(['Volvo', 'Mercedes', 'BMW', 'Ferrari', 'Bazmus'] as const)
+    .optional(),
+  previousOwners: lt.array(PersonDto).default([]),
+  ...EntityDto.shape,
+})
